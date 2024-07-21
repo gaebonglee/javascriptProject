3125a15d8bb94e23fed75822daafe610eb146757
gayoung lee <ken604@naver.com>
2024-07-22 01:27:54 +0900

    요청사항 이슈로 일단 업로드

--- newstimes/news.js
+++ newstimes/news.js
@@ -1,25 +1,89 @@
 const apikey = `bb76162003ae409691da5380af34daf5`;
 let newsList = [];
 const menus = document.querySelectorAll(".menuWrap button");
+const searchInput = document.getElementById("searchInput");
+const today = new Date();
+
+// 상단 날짜 관련
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const dayName = days[today.getDay()];
+const monthName = months[today.getMonth()];
+const day = today.getDate();
+const year = today.getFullYear();
+
+const formattedDate = `${dayName}, ${monthName}, ${day}, ${year}`;
+document.getElementById("currentDate").innerText = formattedDate;
+
 menus.forEach((menu) =>
   menu.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
+// 엔터키 누르면 검색어 입력
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    getNewsByKeyword();
+  }
+});
+
 let url = new URL(
   `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apikey}`
 );
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    url.searchParams.set("page", page);
+    url.searchParams.set("pageSize", pageSize);
 
-  newsList = data.articles;
+    const response = await fetch(url);
 
-  render();
+    const data = await response.json();
+    console.log("ddd", data);
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        if (page > 1) {
+          page--; // 잘못된 페이지로 이동 시 이전 페이지로 돌아가기
+          throw new Error(`No more articles found.`);
+        }
+        throw new Error(
+          `'${searchInput.value}' No articles found for the given keyword.`
+        );
+      }
+      newsList = data.articles;
+      totalResults = data.totalResults;
+      render();
+      pagiNationRender();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message);
+  }
 };
 
 const getLatesNews = async () => {
-  url = new URL();
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apikey}`
+  );
+  page = 1; // 초기 페이지를 1로 설정
   getNews();
 };
 
@@ -29,15 +93,17 @@ const getNewsByCategory = async (event) => {
   url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${apikey}`
   );
+  page = 1; // 카테고리를 변경할 때 페이지 번호를 1로 초기화
   getNews();
 };
 
 const getNewsByKeyword = async () => {
-  const keyword = document.getElementById("searchInput").value;
+  const keyword = searchInput.value;
   console.log("keyword", keyword);
-  const url = new URL(
+  url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${apikey}`
   );
+  page = 1; // 키워드 검색 시 페이지 번호를 1로 초기화
   getNews();
 };
 
@@ -69,9 +135,53 @@ const render = () => {
   document.getElementById("newsBoard").innerHTML = newsHTML;
 };
 
-getNews();
-for (let i = 0; i < 20; i++) {}
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+ ${errorMessage}
+</div>`;
+  document.getElementById("newsBoard").innerHTML = errorHTML;
+};
 
-//1.버튼들에 클릭이벤트 추가
-//2.카테고리별 뉴스 가져오기
-//3. 그 뉴스를 보여주기
+getLatesNews();
+
+// 페이지네이션 제작
+const pagiNationRender = () => {
+  const pageGroup = Math.ceil(page / groupSize);
+  const totalPages = Math.ceil(totalResults / pageSize);
+  let lastPage = pageGroup * groupSize;
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
+
+  const firstPage = Math.max(1, lastPage - (groupSize - 1));
+
+  let pagiNationHTML = ``;
+
+  // Previous 버튼 추가
+  if (page > 1) {
+    pagiNationHTML += `<li class="page-item"><a class="page-link" href="#" onclick="moveToPage(${
+      page - 1
+    })">Previous</a></li>`;
+  }
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    pagiNationHTML += `<li class="page-item ${
+      i === page ? "active" : ""
+    }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  }
+
+  // Next 버튼 추가
+  if (page < totalPages) {
+    pagiNationHTML += `<li class="page-item"><a class="page-link" href="#" onclick="moveToPage(${
+      page + 1
+    })">Next</a></li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = pagiNationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  console.log("moveToPage", pageNum);
+  page = pageNum;
+  getNews();
+};
