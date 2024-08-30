785a65a7cab57fbb309c415d75ed1bcdff4cbe34
gayoung lee <ken604@naver.com>
2024-08-30 23:33:39 +0900

    airbnb 프로젝트

---
+++ airbnb/airbnb.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const typeBtn = document.querySelectorAll(".typeBtn");
+  typeBtn[0].classList.add("active");
+
+  typeBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      typeBtn.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const categoryBtn = document.querySelectorAll(".category-btn");
+  categoryBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      categoryBtn.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const arrowPrev = document.querySelector(".arrowBtn.prev");
+  const arrowNext = document.querySelector(".arrowBtn.next");
+  const categoryBar = document.getElementById("category-bar");
+
+  arrowPrev.addEventListener("click", () => {
+    categoryBar.scrollBy({
+      left: -200,
+      behavior: "smooth",
+    });
+  });
+  arrowNext.addEventListener("click", () => {
+    categoryBar.scrollBy({
+      left: 200,
+      behavior: "smooth",
+    });
+  });
+});
