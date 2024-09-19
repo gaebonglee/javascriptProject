a84e702c8ff531c57fe60fc98fc3f7346fd66594
gayoung lee <ken604@naver.com>
2024-09-19 18:59:13 +0900

    문제해결

--- nextLevelFairs/fairs.js
+++ nextLevelFairs/fairs.js
@@ -1,7 +1,6 @@
-//-----마우스 움직이기-----\\
 document.addEventListener("mousemove", (e) => {
-  const cursorDefaultInner = document.querySelector(".cursor_default_inner");
-  const cursorTraceInner = document.querySelector(".cursor_trace_inner");
+  const cursorDefaultInner = document.querySelector(".cursor__default__inner");
+  const cursorTraceInner = document.querySelector(".cursor__trace__inner");
 
   cursorDefaultInner.style.top = e.clientY + "px";
   cursorDefaultInner.style.left = e.clientX + "px";
@@ -11,32 +10,35 @@ document.addEventListener("mousemove", (e) => {
 });
 
 const cursor = document.querySelector(".cursor");
+
 document.addEventListener("mousedown", () => {
-  cursor.classList.add("cursor_active");
+  cursor.classList.add("cursor--active");
 });
 
 document.addEventListener("mouseup", () => {
-  cursor.classList.remove("cursor_active");
+  cursor.classList.remove("cursor--active");
 });
 
 function createRipple(e) {
   let ripple = document.createElement("span");
+
   ripple.classList.add("ripple");
 
   cursor.appendChild(ripple);
 
   ripple.style.top = e.clientY - ripple.clientHeight / 2 + "px";
-  ripple.style.left = e.clientX - ripple.clientHeight / 2 + "px";
+  ripple.style.left = e.clientX - ripple.clientWidth / 2 + "px";
 
   ripple.addEventListener("animationend", () => {
     cursor.removeChild(ripple);
   });
 }
+
 document.addEventListener("click", (e) => {
   createRipple(e);
 });
 
-const preloaderBtn = document.querySelector(".preloader_btn");
+const preloaderBtn = document.querySelector(".preloader__btn");
 
 let intervalId = null;
 let scale = 1;
@@ -45,7 +47,7 @@ const preloaderHideThreshold = 18;
 
 function setPreloaderStyle(scale) {
   preloaderBtn.style.transform = `scale(${scale})`;
-  document.querySelector(".preloader_btn_hold").style.opacity =
+  document.querySelector(".preloader__btn_hold").style.opacity =
     1 - (scale - 1) / preloaderHideThreshold;
 }
 
@@ -54,11 +56,16 @@ const header = document.querySelector(".header");
 preloaderBtn.addEventListener("mousedown", () => {
   intervalId = setInterval(() => {
     scale += 0.175;
+
     setPreloaderStyle(scale);
+
     if (scale >= 1 + preloaderHideThreshold) {
       document.querySelector(".preloader").classList.add("hidden-area");
+
       header.classList.remove("hidden-area");
-      header.classList.add("show_area");
+
+      header.classList.add("shown-area");
+
       clearInterval(intervalId);
     }
   }, 10);
@@ -66,11 +73,15 @@ preloaderBtn.addEventListener("mousedown", () => {
 
 preloaderBtn.addEventListener("mouseup", () => {
   clearInterval(intervalId);
+
   intervalId = setInterval(() => {
     scale -= 0.075;
+
     setPreloaderStyle(scale);
+
     if (scale <= 1) {
       clearInterval(intervalId);
     }
   }, 10);
 });
+
